2f60b527a537eed4482ae5852632e6dbd79ed1b2
whendev <ururugeorge@gmail.com>
2022-09-15 17:57:21 +0300

    Home Page and adding images

---
+++ src/assets/index.js
@@ -0,0 +1,41 @@
+// Import TVShows Images
+import linkTvOne from "./images/posters/posters-tvshows/breaking-bad.png";
+import linkTvTwo from "./images/posters/posters-tvshows/chernobyl.png";
+import linkTvThree from "./images/posters/posters-tvshows/friends.png";
+import linkTvFour from "./images/posters/posters-tvshows/gotham.png";
+import linkTvFive from "./images/posters/posters-tvshows/sherlock.png";
+import linkTvSix from "./images/posters/posters-tvshows/the-walking-dead.png";
+
+// Import Movies Images
+import linkMvOne from "./images/posters/posters-movies/finch.png";
+import linkMvTwo from "./images/posters/posters-movies/nobody.png";
+import linkMvThree from "./images/posters/posters-movies/shang-chi.png";
+import linkMvFour from "./images/posters/posters-movies/spider-man-no-way-home.png";
+import linkMvFive from "./images/posters/posters-movies/the-tomorrow-war.png";
+import linkMvSix from "./images/posters/posters-movies/wrath-of-man.png";
+
+// Import Serials Images
+import linkSlOne from "./images/posters/posters-series/doctor.png";
+import linkSlTwo from "./images/posters/posters-series/office.png";
+import linkSlThree from "./images/posters/posters-series/picky-blinders.png";
+import linkSlFour from "./images/posters/posters-series/the-big-bang-theory.png";
+import linkSlFive from "./images/posters/posters-series/trigger.png";
+import linkSlSix from "./images/posters/posters-series/yellowstone.png";
+
+// Import Marketing Images
+import soon from "./images/marketing/coming-soon.png";
+import dune from "./images/marketing/dune-partTwo.png";
+
+
+
+// Export TvShows Images
+export { linkTvOne, linkTvTwo, linkTvThree, linkTvFour, linkTvFive, linkTvSix };
+
+// Export Movies Images
+export { linkMvOne, linkMvTwo, linkMvThree, linkMvFour, linkMvFive, linkMvSix };
+
+// Export Serials Images
+export { linkSlOne, linkSlTwo, linkSlThree, linkSlFour, linkSlFive, linkSlSix };
+
+// Export Marketing Images
+export { soon, dune };
---
+++ src/pages/TvShows.js
@@ -0,0 +1,285 @@
+import React from "react";
+
+// Import CSS 
+import "../assets/css/tvshows.css";
+
+class TvShows extends React.Component {
+    render() {
+        return(
+            <div>
+                <header>
+      <div class="header">
+        <a class="header-logo" href="/#">KINOLIFE</a>
+        <nav>
+          <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link" href="./home.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="/#">TV Shows</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./movies.html">Movies</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./serials.html">Serials</a>
+            </li>
+          </ul>
+        </nav>
+        <ul class="control">
+          <li class="control-item search">
+            <form action="" method="get">
+              <input name="s" placeholder="Seacrh" type="search">
+            </form>
+          </li>
+        </ul>
+      </div>
+    </header>
+
+    <section class="section section-tvshows">
+      <div class="container">
+        <h2 class="section__h2">
+          <a class="section__h2-link" href="/#">Trending TV Shows</a>
+        </h2>
+        <div class="movies">
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/breaking-bad.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/the-walking-dead.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/gotham.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/sherlock.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/chernobyl.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/friends.png"
+                alt=""
+            /></a>
+          </div>
+        </div>
+        <div class="movies">
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/breaking-bad.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/the-walking-dead.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/gotham.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/sherlock.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/chernobyl.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/friends.png"
+                alt=""
+            /></a>
+          </div>
+        </div>
+        <div class="movies">
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/breaking-bad.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/the-walking-dead.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/gotham.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/sherlock.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/chernobyl.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="movies-item">
+            <a class="movies__link" href="/#"
+              ><img
+                class="movies__img"
+                src="../images/posters/posters-tvshows/friends.png"
+                alt=""
+            /></a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="marketing">
+          <div class="marketing-item">
+            <a class="marketing__link" href="/#"
+              ><img
+                class="marketing__img"
+                src="../images/marketing/dune-partTwo.png"
+                alt=""
+            /></a>
+          </div>
+          <div class="marketing-item">
+            <a class="marketing__link" href="/#"
+              ><img
+                class="marketing__img"
+                src="../images/marketing/coming-soon.png"
+                alt=""
+            /></a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <div class="footer">
+        <div class="footer-item">
+          <a class="footer-logo" href="/#">KINOLIFE</a>
+        </div>
+        <div class="footer-item">
+          <ul class="footer-list">
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Voice over and Subtitle</a>
+            </li>
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Media Center </a>
+            </li>
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Privacy</a>
+            </li>
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Contact us</a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-item">
+          <ul class="footer-list">
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Voice over and Subtitle</a>
+            </li>
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Investor Relations</a>
+            </li>
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Legal Provisions</a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-item">
+          <ul class="footer-list">
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Help Center</a>
+            </li>
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Job Opportunities</a>
+            </li>
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Cookies Preferences</a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-item">
+          <ul class="footer-list">
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Gift Cards</a>
+            </li>
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Terms of Use </a>
+            </li>
+            <li class="footer-list__item">
+              <a class="footer-list__link" href="/#">Corporate Informations</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+            </div>
+        );
+    }
+}
